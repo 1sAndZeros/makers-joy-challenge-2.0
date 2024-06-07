46585a464da689da2eeb0d5275025f6dfa47353f
Rikie Patrick <rikiepatrick@gmail.com>
2024-06-07 15:25:26 +0100

    update landing and styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.deloittedigital.com",
+        port: "",
+        // pathname: "/account123/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
